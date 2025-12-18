d48219d4e4075aa70b58656386a2d73b5cc5bfca
Zarmina Desktop <zarminaehsan@icloud.com>
2025-12-18 16:35:51 +0100

    A complete rock paper scissors game.

--- src/rps/RockPaperScissors.java
+++ src/rps/RockPaperScissors.java
@@ -1,8 +1,13 @@
+/*
+https://docs.oracle.com/javase/tutorial/displayCode.html?code=https://docs.oracle.com/javase/tutorial/uiswing/examples/components/RadioButtonDemoProject/src/components/RadioButtonDemo.java
+*/
+
 package rps;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Random;
 
 
 public class RockPaperScissors extends JPanel
@@ -14,7 +19,10 @@ public class RockPaperScissors extends JPanel
     static String scissorsString = "Scissors";
 
     // Label to display text
-    static JLabel label;
+    private JLabel informationLabel;
+    private JLabel playerChoiceLabel;
+    private JLabel cpuChoiceLabel;
+    private JLabel resultLabel;
 
     public RockPaperScissors() {
         super(new BorderLayout());
@@ -23,7 +31,6 @@ public class RockPaperScissors extends JPanel
         JRadioButton rockButton = new JRadioButton(rockString);
         rockButton.setMnemonic(KeyEvent.VK_R);
         rockButton.setActionCommand(rockString);
-        rockButton.setSelected(true);
 
         JRadioButton paperButton = new JRadioButton(paperString);
         paperButton.setMnemonic(KeyEvent.VK_P);
@@ -32,6 +39,9 @@ public class RockPaperScissors extends JPanel
         JRadioButton scissorsButton = new JRadioButton(scissorsString);
         scissorsButton.setMnemonic(KeyEvent.VK_S);
         scissorsButton.setActionCommand(scissorsString);
+
+        // Select one by default
+        //rockButton.setSelected(true);
 
         //Group the radio buttons.
         ButtonGroup group = new ButtonGroup();
@@ -44,13 +54,21 @@ public class RockPaperScissors extends JPanel
         paperButton.addActionListener(this);
         scissorsButton.addActionListener(this);
 
-        //Put the radio buttons in a column in a panel.
-        JPanel radioPanel = new JPanel(new GridLayout(4, 0));
-        radioPanel.add(label);
+        // Creating a label to display text
+        informationLabel = new JLabel("Choose rock, paper or scissors");
+        playerChoiceLabel = new JLabel("Player chose:");
+        cpuChoiceLabel = new JLabel("CPU chose:");
+        resultLabel = new JLabel("Winner:");
+
+        //Put the radio buttons and label in a column in a panel.
+        JPanel radioPanel = new JPanel(new GridLayout(0, 1));
+        radioPanel.add(informationLabel);
         radioPanel.add(rockButton);
         radioPanel.add(paperButton);
         radioPanel.add(scissorsButton);
-
+        radioPanel.add(playerChoiceLabel);
+        radioPanel.add(cpuChoiceLabel);
+        radioPanel.add(resultLabel);
 
         add(radioPanel, BorderLayout.LINE_START);
         setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
@@ -58,7 +76,32 @@ public class RockPaperScissors extends JPanel
 
     /** Listens to the radio buttons. */
     public void actionPerformed(ActionEvent e) {
-        e.getActionCommand();
+        String playerChoice = e.getActionCommand();
+        System.out.println("Player chose" + playerChoice);
+
+        playerChoiceLabel.setText("Player chose " + playerChoice);
+
+        String[] cpuChoiceList = {rockString,paperString,scissorsString};
+
+        Random random = new Random();
+
+        int randomIndex = random.nextInt(cpuChoiceList.length);
+        String cpuChoice = cpuChoiceList[randomIndex];
+        System.out.println("CPU chose " + playerChoice);
+        cpuChoiceLabel.setText("CPU chose " + cpuChoice);
+
+        if(playerChoice.equals(cpuChoice)){
+            resultLabel.setText("It's a Draw.");
+        }
+        else if ( playerChoice.equals(rockString) && cpuChoice.equals(scissorsString) ||
+                    playerChoice.equals(paperString) && cpuChoice.equals(rockString) ||
+                    playerChoice.equals(scissorsString) && cpuChoice.equals(paperString)) {
+            resultLabel.setText("Player wins.");
+        }
+        else {
+            resultLabel.setText("CPU wins.");
+        }
+
     }
 
     /**
@@ -68,20 +111,19 @@ public class RockPaperScissors extends JPanel
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("Rock Paper Scissors");
+        JFrame frame = new JFrame("R-P-S");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Creating a label to display text
-        label = new JLabel("Choose rock, paper or scissors");
+        frame.setSize(600, 300);
 
         //Create and set up the content pane.
-        JComponent newContentPane = new RockPaperScissors();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
+        JComponent newContentPanel = new RockPaperScissors();
+        newContentPanel.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPanel);
 
         //Display the window.
         frame.pack();
         frame.setVisible(true);
+
     }
 
     public static void main(String[] args){
